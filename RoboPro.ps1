431065e2116a64d1fafea6f5b6013a0a20b1b340
dazbuttars <dbuttars12@gmail.com>
2022-04-11 15:29:18 -0600

    Add files via upload

---
+++ RoboPro.ps1
@@ -0,0 +1,7 @@
+﻿$oldPro = Read-Host 'Old Profile location [ENTER]'
+$newPro = Read-Host 'New Profile location [ENTER]'
+
+Robocopy $oldPro $newPro /E /r:5 /w:3 /xd "$oldPro\AppData" /xd "$oldPro\Local Settings"
+Robocopy "$oldPro\AppData\Local\Google\Chrome" "$newPro\AppData\Local\Google\Chrome" /E /r:5 /w:3
+
+$a = Read-Host 'Hit Enter to Exit'
